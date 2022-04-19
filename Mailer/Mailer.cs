999a11528ffac9cfb30fdeeaf22471ecbf6b9f15
emily <emily@DESKTOP-OI9P2BU>
2022-04-19 16:38:54 -0400

    Add project files.

---
+++ Mailer/Mailer.cs
@@ -0,0 +1,71 @@
+﻿using MailKit;
+using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Mailer    
+{
+    public class Mailer
+    {
+        public static async Task<int> SendEmail(string sender, string to, string addr, string subject, string body)
+        {
+            
+            //Import credentials from appsettings.json
+            string url, port, username, password;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            url = configuration["url"];
+            port = configuration["port"];
+            username = configuration["username"];
+            password = configuration["password"];
+
+            string status = "failed";
+
+            var message = new MimeMessage();
+
+                message.From.Add(new MailboxAddress(sender, username)); //the second field is never used, but displays weird in my inbox if left empty
+                message.To.Add(new MailboxAddress(to, addr));
+                message.Subject = subject;
+                message.Body = new TextPart("plain") { Text = body };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect(url, int.Parse(port));
+                    client.Authenticate(username, password);
+
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        try
+                        {
+                            Console.WriteLine("sending");
+                            status = client.Send(message);
+                            Console.WriteLine(status);
+                            break;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    //TODO: Log email and final status
+                    using StreamWriter file = new("log.txt", append: true);
+                string logEntry = DateTime.Now.ToString() + "\t"
+                    + sender + ", " + username + "\t"
+                    + to + ", " + addr + "\t"
+                    + subject + "\t"
+                    + body + "\t"
+                    + status + "\n";
+                await file.WriteLineAsync(logEntry);
+
+                    client.Disconnect(true);
+                }
+            return 0;  
+        }
+    }
+}
---
+++ MailerCLI/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mailer;
+
+const string receiver = "replace.me@bademail.com";
+
+Task<int> mailStatus = null;
+
+
+
+//Main Loop
+while (true)
+{
+    string send, rec, sub, bod, cmd, to;
+    Console.WriteLine("Awaiting instructions...");
+    cmd = Console.ReadLine();
+    if (cmd.Equals("exit"))
+    {
+        break;
+    } else if (cmd.Equals("send")){
+        Console.WriteLine("Enter sender name:");
+        send = Console.ReadLine();
+        Console.WriteLine("Enter recipient name:");
+        to = Console.ReadLine();
+        Console.WriteLine("Enter recipient address:");
+        rec = Console.ReadLine();
+        Console.WriteLine("Enter subject:");
+        sub = Console.ReadLine();
+        Console.WriteLine("Enter message body:");
+        bod = Console.ReadLine();
+        Console.WriteLine("To: "+ to + ", " + rec + "\nFrom: " + send + "\nSubject: " + sub + "\nBody: " + bod + "\nConfirm Y/N?");
+        cmd = Console.ReadLine();
+        if (cmd.ToLower().Equals("y")){
+            mailStatus = Mailer.Mailer.SendEmail(send, to, rec, sub, bod);
+            Console.WriteLine("request sent");
+        } else continue;
+    } else if (cmd.Equals("qt"))
+    {
+        mailStatus = Task.Run(() => Mailer.Mailer.SendEmail("Rob Ott", "Emily", receiver, "Rapid Test Email", "The version of the test for sending a quick message without manual entry."));
+        Console.WriteLine("request sent");
+    }
+}
+if(mailStatus != null) await mailStatus;
